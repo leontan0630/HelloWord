95922b2f872473b67f020b27d0345f3b9bff3867
kuye1210 <kuye1210@hotmail.com>
2016-11-05 15:15:36 -0400

    ADD COMPONENTS

--- src/helloWord/frame.java
+++ src/helloWord/frame.java
@@ -1,4 +1,3 @@
-
 package helloWord;
 import java.awt.*;
 import javax.swing.*;
@@ -6,14 +5,34 @@ import javax.swing.*;
 public class frame extends JFrame {
 
     private static final long serialVersionUID = 1L;
+    private JTextField textField;
 
     public frame() {
         Box box = new Box(BoxLayout.Y_AXIS);
         box.setAlignmentX(JComponent.CENTER_ALIGNMENT);
         box.add(Box.createVerticalGlue());
-        box.add(new mainFrame());
+        helloWord.mainFrame mainFrame_ = new mainFrame();
+        box.add(mainFrame_);
+        mainFrame_.setLayout(null);
+        
+        textField = new JTextField();
+        textField.setBounds(80, 49, 542, 75);
+        mainFrame_.add(textField);
+        textField.setColumns(10);
+        
+        JButton btnTranslate = new JButton("TRANSLATE");
+        btnTranslate.setBounds(284, 153, 122, 46);
+        mainFrame_.add(btnTranslate);
+        
+        JLabel lblNewLabel = new JLabel("New label");
+        lblNewLabel.setBounds(80, 233, 542, 80);
+        mainFrame_.add(lblNewLabel);
+        
+        JLabel lblNewLabel_1 = new JLabel("New label");
+        lblNewLabel_1.setBounds(80, 349, 542, 75);
+        mainFrame_.add(lblNewLabel_1);
         box.add(Box.createVerticalGlue());
-        add(box);
+        getContentPane().add(box);
         pack();
         setTitle("Hello Word");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -43,17 +62,17 @@ class mainFrame extends JPanel {
 
     @Override
     public Dimension getMinimumSize() {
-        return new Dimension(400, 400);
+        return new Dimension(700, 700);
     }
 
     @Override
     public Dimension getMaximumSize() {
-        return new Dimension(400, 400);
+        return new Dimension(700, 700);
     }
 
     @Override
     public Dimension getPreferredSize() {
-        return new Dimension(400, 400);
+        return new Dimension(700, 700);
     }
 
    /* @Override
